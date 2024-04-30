b1518986942da40f453056c0ae1469ce4fdd83af
Micka <mickael.murmann@gmail.com>
2024-04-30 12:44:31 +0200

    Fix CSS in latest topics list (responsive)

--- app/(forum)/(topics)/topics/[categoryId]/_components/TopicsPageLayout.tsx
+++ app/(forum)/(topics)/topics/[categoryId]/_components/TopicsPageLayout.tsx
@@ -31,12 +31,12 @@ const TopicsPageLayout = async ({
             <div className="flex-1">
                 {children}
             </div>
-            <div className="bg-slate-200 mb-4 md:mb-0 min-w-[400px] px-8 py-8 md:px-8 md:py-8 ">
+            <div className="bg-slate-200 mb-4 md:mb-0 min-w-[400px] px-8 py-8 md:px-12 md:py-8 ">
                 <h2 className="text-xl font-semibold mb-8">Latest topics in {category?.name}</h2>
                 <div className="flex flex-col gap-y-3">
                     {lastTopics.map((topic) => (
                         <Link key={topic.id} href={`/posts/${topic.id}`}>
-                            <div className="p-4 rounded-lg bg-white shadow-md gap-x-2 hover:bg-slate-100">
+                            <div className="p-6 rounded-lg bg-white shadow-md gap-x-2 hover:bg-slate-100">
                                 <p className="font-medium">{topic.title}</p>
                                 <p className="text-xs">{formatDateTime(topic.createdAt)} by {topic.userId}</p>
                             </div>
