b4e83534200fc605a52ff728bfb3496c00438661
Micka <mickael.murmann@gmail.com>
2024-04-30 12:19:04 +0200

    Design sidebar with latest topics (specific category)

--- app/(forum)/(topics)/topics/[categoryId]/_components/TopicsPageLayout.tsx
+++ app/(forum)/(topics)/topics/[categoryId]/_components/TopicsPageLayout.tsx
@@ -1,4 +1,5 @@
 import { db } from "@/lib/db";
+import { formatDateTime } from "@/lib/format-datetime";
 import Link from "next/link";
 
 const TopicsPageLayout = async ({
@@ -20,16 +21,19 @@ const TopicsPageLayout = async ({
     })
 
     return (
-        <div className="flex">
+        <div className="flex flex-col-reverse lg:flex-row">
             <div className="flex-1">
                 {children}
             </div>
-            <div className="bg-slate-200 min-w-[200px] p-6">
-                <h2 className="font-semibold">Latest topics</h2>
-                <div className="flex flex-col">
+            <div className="bg-slate-200 mb-4 md:mb-0 min-w-[400px] px-8 py-8 md:px-8 md:py-8 ">
+                <h2 className="text-xl font-semibold mb-4">Latest topics</h2>
+                <div className="flex flex-col gap-y-3">
                     {lastTopics.map((topic) => (
                         <Link key={topic.id} href={`/posts/${topic.id}`}>
-                            {topic.title}
+                            <div className="p-4 rounded-lg bg-white shadow-md gap-x-2 hover:bg-slate-100">
+                                <p className="font-medium">{topic.title}</p>
+                                <p className="text-xs">{formatDateTime(topic.createdAt)} by {topic.userId}</p>
+                            </div>
                         </Link>
                     ))}
                 </div>
